91c8211781013399c267afd88bccfca385b4d067
Juan Aguirre <j1927aguirre@gmail.com>
2021-01-05 00:18:27 -0500

    Add TypeORM configuration, Database connection and NPM packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonModule } from './lesson/lesson.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
   controllers: [],
   providers: [],
